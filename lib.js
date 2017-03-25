5cf728ddaadd45843dae1d0ed41beb0cacae963a
Rudraksh MK <rudraksh@paperkite.in>
2017-03-25 11:55:09 +0530

    Storing an extra  property.

--- lib.js
+++ lib.js
@@ -7,9 +7,9 @@ module.exports = class Dua {
     this.id = uuid.v4()
     this.language = language
     this.message = `Blessing ID #${ this.id }: Our blessings are with you.`
-    this.translation = null
+    this.translation
     this.translateBlessing = () => translator('Our blessings are with you.', {to: this.language}).then(res => 
-      {return `Blessing ID #${ this.id }: ${ res.text }`})
+      {this.translation = `Blessing ID #${ this.id }: ${ res.text }`; return `Blessing ID #${ this.id }: ${ res.text }`})
     this.blessThisCode = () => this.message
     this.isCodeBlessed = () => true
   }
