df13d39efb35c8c24c5298d77cd1e7ed7b15c8f0
Rudraksh MK <rudraksh@paperkite.in>
2017-03-25 11:04:53 +0530

    Added a class method that uses the Google Translate API to create a translation for the blessing message.

--- lib.js
+++ lib.js
@@ -1,11 +1,21 @@
 'use strict';
 const uuid = require('uuid')
+const translator = require('google-translate-api')
 
 module.exports = class Dua {
-  constructor () {
+  constructor (language) {
     this.id = uuid.v4()
+    this.language = language
     this.message = `Blessing ID #${ this.id }: Our blessings are with you.`
+    this.translation = null
     this.blessThisCode = () => this.message
     this.isCodeBlessed = () => true
+  }
+
+  translate(){
+    translator('Our blessings are with you.', {to: this.language})
+    .then(res => {
+      this.translation = `Blessing ID #${ this.id }: ${ res.text }`
+    })
   }
 }
