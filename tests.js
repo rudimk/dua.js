5cb97cff15240dd010a97d6d7ffd2df5054ec2c0
Rudraksh MK <rudraksh@paperkite.in>
2017-03-25 11:43:20 +0530

    Added a test case to check if translations work.

--- tests.js
+++ tests.js
@@ -1,21 +1,33 @@
 var expect = require('chai').expect
+//var chaiAsPromised = require("chai-as-promised")
 var dua = require('./lib.js')
 
+//chai.use(chaiAsPromised)
+
 describe("Blessing generator", function(){
-    it("generates a new blessing", function(){
-        var testDua = new dua()
+    var testDua = new dua('fr')
+    
+    it("Generate a new blessing", function(){
         console.log(`Blessing ID ==> ${testDua.id}`)
         console.log(`Blessing ==> ${testDua.message}`)
 
         expect(testDua.id).to.be.a('string')
         expect(testDua.message).to.be.a('string')
     })
-    it("check if one's code is blessed", function(){
-        var testDua = new dua()
+    it("Check if one's code is blessed", function(){
         console.log(`Blessing ID ==> ${testDua.id}`)
         console.log(`Blessing ==> ${testDua.message}`)
         console.log(`Blessing status ===> ${testDua.isCodeBlessed()}`)
 
         expect(testDua.isCodeBlessed()).equals(true)
+    })
+    it("Check if translations work", function(){
+        console.log(`Blessing ID ==> ${testDua.id}`)
+        console.log(`Blessing ==> ${testDua.message}`)
+        console.log(`Blessing language ===> ${testDua.language}`)
+        testDua.translateBlessing().then(function(translation){
+            expect(translation).to.be.a('string')
+        })    
+        
     })
 })
