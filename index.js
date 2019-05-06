a3d1e5e2dcfc46cb99fe893fdea007b85896b769
Rudraksh Mukta Kulshreshtha <rudimk.cloud@gmail.com>
2019-05-06 16:12:47 +0530

    Removed constructor argument and class attribute initialization for language support.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-'use strict';
 const uuid = require('uuid')
 
 module.exports = class Dua {
-  constructor (language) {
+  constructor () {
     this.id = uuid.v4()
-    this.language = language
     this.message = `Blessing ID #${ this.id }: Our blessings are with you.`
     this.blessThisCode = () => this.message
     this.isCodeBlessed = () => true
