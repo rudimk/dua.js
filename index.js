0eafe2e925061104c7db4e25ee73be92559a88f5
Rudraksh Mukta Kulshreshtha <rudimk.cloud@gmail.com>
2019-05-06 16:11:15 +0530

    Removed code for translating blessings.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict';
 const uuid = require('uuid')
-const translator = require('google-translate-api')
 
 module.exports = class Dua {
   constructor (language) {
     this.id = uuid.v4()
     this.language = language
     this.message = `Blessing ID #${ this.id }: Our blessings are with you.`
-    this.translation
-    this.translateBlessing = () => translator('Our blessings are with you.', {to: this.language}).then(res => 
-      {this.translation = `Blessing ID #${ this.id }: ${ res.text }`; return `Blessing ID #${ this.id }: ${ res.text }`})
     this.blessThisCode = () => this.message
     this.isCodeBlessed = () => true
   }
